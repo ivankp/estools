c9870d42ccbf98fc6b35232b3aa01d2de063412e
ivankp <ivan.pogrebnyak@gmail.com>
2019-12-01 03:21:59 -0500

    edited map script

--- map.py
+++ map.py
@@ -7,12 +7,6 @@ if len(sys.argv)!=3:
     sys.exit(1)
 
 import codecs
-
-from lxml import etree as Tree
-with open(sys.argv[1]) as f:
-    tree = Tree.parse(f)
-    root = tree.getroot()
-
 def decode(s):
     if s is None or s=='':
         return b'';
@@ -20,14 +14,13 @@ def decode(s):
 
 import struct, zlib
 # https://stackoverflow.com/a/19174800/2640636
-def write_png(buf, width, height):
+def make_png(buf, width, height):
     """ buf: must be bytes or a bytearray in Python3.x,
         a regular string in Python2.x.
     """
 
-    # reverse the vertical line order and add null bytes at the start
     width_byte_4 = width * 4
-    raw_data = b''.join(
+    raw_data = b''.join( # add null bytes at the start of each line
         b'\x00' + buf[i:i + width_byte_4]
         for i in range(0, height * width_byte_4, width_byte_4)
     )
@@ -47,12 +40,18 @@ def write_png(buf, width, height):
 def add4th(s):
     return b''.join(s[i:i+3]+b'\xff' for i in range(0,len(s),3))
 
+from lxml import etree as Tree
+with open(sys.argv[1]) as f:
+    tree = Tree.parse(f)
+    root = tree.getroot()
+
 size = int(root.xpath('(/file/FMAP/MAPH/x)[1]')[0].text)
 mapd = root.xpath('(/file/FMAP/MAPD/x)[1]')[0].text
 mapd = decode(mapd)
-print(len(mapd))
+print('raw size:',len(mapd))
+png = make_png(add4th(mapd),size,size)
+print('png size:',len(png))
 
 with open(sys.argv[2],'wb') as f:
-    # f.write(mapd)
-    f.write(write_png(add4th(mapd),size,size))
+    f.write(png)
 
